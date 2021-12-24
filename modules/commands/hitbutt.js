8925f5544d5cca400272c82d8a9d2150877146ec
Hoàng Đỗ Gia Nguyên <90612922+Shiron-Official@users.noreply.github.com>
2021-12-24 21:01:34 +0700

    Update hitbutt.js

--- modules/commands/hitbutt.js
+++ modules/commands/hitbutt.js
@@ -5,7 +5,7 @@
 
 module.exports.config = {
     name: "hitbutt",
-    version: "2.2.2",
+    version: "2.2.6",
     hasPermssion: 0,
     credits: "ProCoderMew",
     description: "",
@@ -17,6 +17,9 @@ module.exports.config = {
         "fs-extra": "",
         "path": "",
         "jimp": ""
+    },
+    envConfig: {
+        APIKEY: ""
     }
 };
 
@@ -27,10 +30,11 @@ module.exports.onLoad = async() => {
     const dirMaterial = __dirname + `/cache/canvas/`;
     const path = resolve(__dirname, 'cache/canvas', 'hit_butt.png');
     if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://git.meewmeew.info/data/hitbutt.png", path);
+    if (!existsSync(path)) await downloadFile("https://dev.meewmeew.info/Module-Miraiv2/data/hitbutt.png", path);
 }
 
 async function makeImage({ one, two }) {
+    const { APIKEY } = global.configModule.hitbutt;
     const fs = global.nodemodule["fs-extra"];
     const path = global.nodemodule["path"];
     const axios = global.nodemodule["axios"]; 
@@ -39,14 +43,20 @@ async function makeImage({ one, two }) {
 
     let hit_butt_img = await jimp.read(__root + "/hit_butt.png");
     let pathImg = __root + `/hit_butt_${one}_${two}.png`;
-    let avatarOne = (await axios.get(`https://meewmeew.info/avatar/${one}`)).data;    
-    let avatarTwo = (await axios.get(`https://meewmeew.info/avatar/${two}`)).data;    
-    let circleOne = await jimp.read(await circle(Buffer.from(avatarOne, 'utf-8')));
-    let circleTwo = await jimp.read(await circle(Buffer.from(avatarTwo, 'utf-8')));
+
+    try {
+        var avatarOne = (await axios.get(`https://meewmeew.info/avatar/${one}?apiey=${APIKEY}`)).data;    
+        var avatarTwo = (await axios.get(`https://meewmeew.info/avatar/${two}?apiey=${APIKEY}`)).data;
+        var circleOne = await jimp.read(await circle(Buffer.from(avatarOne, 'utf-8')));
+        var circleTwo = await jimp.read(await circle(Buffer.from(avatarTwo, 'utf-8')));        
+    } catch {
+        let raw = await hit_butt_img.getBufferAsync("image/png");    
+        fs.writeFileSync(pathImg, raw);
+        return pathImg;
+    }    
     hit_butt_img.resize(500, 500).composite(circleOne.resize(130, 130), 225, 5).composite(circleTwo.resize(120, 120), 352, 220);
     
-    let raw = await hit_butt_img.getBufferAsync("image/png");
-    
+    let raw = await hit_butt_img.getBufferAsync("image/png");    
     fs.writeFileSync(pathImg, raw);
     return pathImg;
 }
@@ -57,7 +67,7 @@ async function circle(image) {
     return await image.getBufferAsync("image/png");
 }
 
-module.exports.run = async function ({ event, api, args }) {
+module.exports.run = async function ({ event, api, args }) {    
     const fs = global.nodemodule["fs-extra"];
     const { threadID, messageID, senderID } = event;
     const mention = Object.keys(event.mentions);
