05a607cbcaa11c9826eb675d21416807707dd3bd
Hoàng Đỗ Gia Nguyên <90612922+Shiron-Official@users.noreply.github.com>
2022-01-07 19:17:00 +0700

    Update dovui.js

--- modules/commands/dovui.js
+++ modules/commands/dovui.js
@@ -74,7 +74,7 @@ module.exports.handleReaction = ({ Users, api, event, handleReaction,  }) => {
 module.exports.run = async function ({ api, event, Users }) {
 const axios = global.nodemodule['axios'];  
 const { threadID, messageID } = event;
-const res = await axios.get(`https://simsimi.info/v1/dovui.php?apikey=leanhtruong`);
+const res = await axios.get(`http://api.leanhtruong.net/api/dovui?apikey=điền api key tại đây?(reg api key tại web leanhtruong.net)`);
 const dataGame = res.data
 var namePlayer_react = await Users.getData(event.senderID)
         var msg = {body: `❔Câu hỏi dành cho bạn: ${dataGame.questions}\n\n👍/A. ${dataGame.a}\n❤️/B. ${dataGame.b}\n😆/C. ${dataGame.c}\n😮/D. ${dataGame.d}\n\n🌻Reply tin nhắn hoặc thả cảm xúc để trả lời`}
