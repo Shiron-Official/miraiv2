cd2dc5b777b90ae1275a56430c90c17c19faa76b
Hoàng Đỗ Gia Nguyên <90612922+Shiron-Official@users.noreply.github.com>
2022-01-07 20:40:22 +0700

    Update sing.js

--- modules/commands/sing.js
+++ modules/commands/sing.js
@@ -2,122 +2,134 @@ module.exports.config = {
     name: "sing",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "D-Jukie",
+    credits: "MạnhG",
     description: "Phát nhạc thông qua link YouTube hoặc từ khoá tìm kiếm",
     commandCategory: "Phương tiện",
-    usages: "[search/url youtube]",
-    cooldowns: 10,
+    usages: "[searchVideos]",
+    cooldowns: 5,
     envConfig: {
-        "YOUTUBE_API": "AIzaSyDX4b7tIAgK0GaFMbsA-nLrm36s3Q_93bo"
+        "ManhG_KEY": "ĐIỀN KEY VÀO ĐÂY?(REG KEY TẠI WEB mzkapi.me)"
     }
 };
-
-module.exports.handleReply = async function ({
-    api,
-    event,
-    handleReply
-}) {
-    const axios = require('axios')
+var random = '12345ABCDE',
+    number = 10,
+    rdus = "singManhG_";
+for (var i = 0; i < number; i++) {
+    rdus += random.charAt(Math.floor(Math.random() * random.length));
+}
+module.exports.run = async function({ event, api, args, body }) {
+    const axios = require("axios");
     const fs = require("fs-extra");
-    const request = require("request");
-    const {
-        createReadStream,
-        createWriteStream,
-        unlinkSync,
-        statSync
-    } = global.nodemodule["fs-extra"];
+    var ManhG_KEY = global.configModule[this.config.name].ManhG_KEY;
     try {
-        const result = await axios.get(`https://www.phamvandienofficial.xyz/sing?link=https://www.youtube.com/watch?v=${handleReply.link[event.body - 1]}`);
-        path1 = __dirname + `/cache/${event.senderID}.m4a`
-        const getms = (await axios.get(`${result.data.link}`, {
-            responseType: "arraybuffer"
-        })).data;
-        fs.writeFileSync(path1, Buffer.from(getms, "utf-8"));
-        api.unsendMessage(handleReply.messageID)
-        if (fs.statSync(__dirname + `/cache/${event.senderID}.m4a`).size > 26000000) return api.sendMessage('Không thể gửi file vì dung lượng lớn hơn 100MB.', event.threadID, () => unlinkSync(__dirname + `/cache/${event.senderID}.m4a`), event.messageID);
-        else return api.sendMessage({
-            body: `${result.data.title}`,
-            attachment: fs.createReadStream(__dirname + `/cache/${event.senderID}.m4a`)
-        }, event.threadID, () => fs.unlinkSync(__dirname + `/cache/${event.senderID}.m4a`), event.messageID)
-    } catch {
-        return api.sendMessage('Không thể xử lý yêu cầu của bạn!', event.threadID, event.messageID);
+        const { createReadStream, createWriteStream, unlinkSync, statSync } = require("fs-extra");
+        let key_api = (await axios.get(`http://mzkapi.me/adminkey?key=${ManhG_KEY}`)).data;
+        if (key_api.status != true) return api.sendMessage(`${key_api.msg}`, event.threadID, event.messageID);
+        if (args.length == 0 || !args) return api.sendMessage('» Search cannot be left blank!', event.threadID, event.messageID);
+        if (args.join(" ").indexOf("https://") == 0) {
+            const linkurl = args.join(" ").trim();
+            try {
+                let link, desc, resErr, dataErr;
+                let res = await axios.get(`http://mzkapi.me/sing?link=${linkurl}&apikey=${ManhG_KEY}`);
+                let data = res.data;
+                if (data.link == undefined) {
+                    resErr = await axios.get(`http://mzkapi.me/video?link=${linkurl}&apikey=${ManhG_KEY}`);
+                    dataErr = resErr.data;
+                    desc = dataErr.title;
+                    link = dataErr.link.audio[0];
+                } else {
+                    desc = data.title;
+                    link = data.link;
+                }
+                var path1 = __dirname + `/cache/${rdus}.mp3`;
+                const getms = (await axios.get(link.trim(), { responseType: "arraybuffer" })).data;
+                fs.writeFileSync(path1, Buffer.from(getms, "utf-8"));
+                if (fs.statSync(__dirname + `/cache/${rdus}.mp3`).size > 1164000000) return api.sendMessage('File cannot be sent because it is larger than 500MB.', event.threadID, () => unlinkSync(__dirname + `/cache/${rdus}.mp3`), event.messageID);
+                else return api.sendMessage({ body: desc, attachment: fs.createReadStream(__dirname + `/cache/${rdus}.mp3`) }, event.threadID, () => fs.unlinkSync(__dirname + `/cache/${rdus}.mp3`), event.messageID)
+            } catch (e) {
+                console.log(e);
+                return api.sendMessage('Your request could not be processed!', event.threadID, event.messageID);
+            }
+        } else {
+            try {
+                var results = [],
+                    link = [],
+                    msg = "",
+                    num = 0,
+                    value;
+                const keywordSearch = encodeURIComponent(args.join(" "));
+                results = (await axios.get(`http://mzkapi.me/youtube?q=${keywordSearch}&apikey=${ManhG_KEY}`)).data.results;
+                for (let key in results) {
+                    if (results[key].video != undefined) {
+                        value = (results[key].video);
+                        if (value.duration.length < 5 && value.duration != "Live") {
+                            num = num += 1;
+                            link.push(value.id);
+                            let time = value.duration;
+                            msg += (`${num}.《${time}》 ${value.title}\n\n`);
+                        }
+                    }
+                }
+                var body = `»🔎 There are ${link.length} results matching your search keyword:\n\n${msg}» Please reply (feedback) choose one of the above searches.`;
+                return api.sendMessage({
+                        body: body
+                    }, event.threadID, (error, info) => {
+                        client.handleReply.push({
+                            name: this.config.name,
+                            messageID: info.messageID,
+                            author: event.senderID,
+                            idYT: link
+                        })
+                    },
+                    event.messageID);
+            } catch (error) {
+                return api.sendMessage("The request could not be processed due to an error: " + error.message, event.threadID, event.messageID);
+            }
+        }
+    } catch (ex) {
+        console.log(ex);
+        return;
     }
-    return api.unsendMessage(handleReply.messageID);
 }
-module.exports.run = async function ({
-    api,
-    event,
-    args
-}) {
+
+module.exports.handleReply = async function({ event, api, handleReply }) {
     const axios = require('axios')
     const fs = require("fs-extra");
     const request = require("request");
-    const ytdl = global.nodemodule["ytdl-core"];
-    const YouTubeAPI = global.nodemodule["simple-youtube-api"];
-    const {
-        createReadStream,
-        createWriteStream,
-        unlinkSync,
-        statSync
-    } = global.nodemodule["fs-extra"];
-    const youtube = new YouTubeAPI(global.configModule[this.config.name].YOUTUBE_API);
-    const keyapi = global.configModule[this.config.name].YOUTUBE_API
-    if (args.length == 0 || !args) return api.sendMessage('» Phần tìm kiếm không được để trống!', event.threadID, event.messageID);
-    const keywordSearch = args.join(" ");
-    if (args.join(" ").indexOf("https://") == 0) {
-        var url = args.join(" ")
-        var urlsplit = url.split(/^.*(youtu.be\/|v\/|embed\/|watch\?|youtube.com\/user\/[^#]*#([^\/]*?\/)*)\??v?=?([^#\&\?]*).*/);
-        const linkUrlSing = urlsplit[3]
-        const result = await axios.get(`https://www.phamvandienofficial.xyz/sing?link=https://www.youtube.com/watch?v=${linkUrlSing}`);
-        path1 = __dirname + `/cache/${event.senderID}.m4a`
-        const getms = (await axios.get(`${result.data.link}`, {
-            responseType: "arraybuffer"
-        })).data;
-        fs.writeFileSync(path1, Buffer.from(getms, "utf-8"));
-        if (fs.statSync(__dirname + `/cache/${event.senderID}.m4a`).size > 26000000) return api.sendMessage('Không thể gửi file vì dung lượng lớn hơn 25MB.', event.threadID, () => unlinkSync(__dirname + `/cache/${event.senderID}.m4a`), event.messageID);
-        else return api.sendMessage({
-            body: `» ${result.data.title}`,
-            attachment: fs.createReadStream(__dirname + `/cache/${event.senderID}.m4a`)
-        }, event.threadID, () => fs.unlinkSync(__dirname + `/cache/${event.senderID}.m4a`), event.messageID)
-    } else {
-        try {
-            var link = [],
-                msg = "",
-                num = 0,
-                numb = 0;
-            var imgthumnail = [];
-            var results = await youtube.searchVideos(keywordSearch, 6);
-            for (let value of results) {
-                if (typeof value.id == 'undefined') return;
-                link.push(value.id);
-                var idd = value.id;
-                let datab = (await axios.get(`https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id=${value.id}&key=${keyapi}`)).data;
-                let gettime = datab.items[0].contentDetails.duration;
-                let timeee = (gettime.slice(2));
-                let timee = timeee.replace('S', '')
-                let time = timee.replace('M', ':')
-                num = num+=1
-                if (num == 1) var num1 = "⓵"
-                if (num == 2) var num1 = "⓶"
-                if (num == 3) var num1 = "⓷"
-                if (num == 4) var num1 = "⓸"
-                if (num == 5) var num1 = "⓹"
-                if (num == 6) var num1 = "⓺"
-                msg += (`${num1} 《${time}》 ${value.title}\n\n`);
-            }
-            var body = `»🔎 Có ${link.length} kết quả trùng với từ khoá tìm kiếm của bạn:\n\n${msg}» Hãy reply(phản hồi) chọn một trong những tìm kiếm trên`
+    var ManhG_KEY = global.configModule[this.config.name].ManhG_KEY;
+    const { createReadStream, createWriteStream, unlinkSync, statSync } = require("fs-extra");
+    var rqx = event.body;
 
-            return api.sendMessage({
-                    body: body
-                }, event.threadID, (error, info) => global.client.handleReply.push({
-                    name: this.config.name,
-                    messageID: info.messageID,
-                    author: event.senderID,
-                    link
-                }),
-                event.messageID);
-        } catch {
-            return api.sendMessage('Đã xảy ra lỗi!!', event.threadID, event.messageID)
+    function number(x) {
+        if (isNaN(x)) {
+            return 'Not a Number!';
         }
+        return (x < 1 || x > 20);
+    }
+    if (number(rqx)) return api.sendMessage('Choose from 1 -> 20, baby. love uwu ❤️', event.threadID, event.messageID);
+    api.unsendMessage(handleReply.messageID);
+    api.sendMessage(`Audio processing, please wait...`, event.threadID, (err, info) => setTimeout(() => { api.unsendMessage(info.messageID) }, 20000));
+    try {
+        let link, desc;
+        let res = await axios.get(`http://mzkapi.me/sing?link=http://youtu.be/${handleReply.idYT[event.body - 1]}&apikey=${ManhG_KEY}`);
+        let data = res.data;
+        if (data.link == undefined) {
+            let resErr = await axios.get(`http://mzkapi.me/video?link=http://youtu.be/${handleReply.idYT[event.body - 1]}&apikey=${ManhG_KEY}`);
+            let dataErr = resErr.data;
+            desc = dataErr.title;
+            link = dataErr.link.audio[0];
+        } else {
+            desc = data.title;
+            link = data.link;
+        }
+        var path1 = __dirname + `/cache/${rdus}.mp3`;
+        const getms = (await axios.get(link.trim(), { responseType: "arraybuffer" })).data;
+        fs.writeFileSync(path1, Buffer.from(getms, "utf-8"));
+        if (fs.statSync(__dirname + `/cache/${rdus}.mp3`).size > 104000000) return api.sendMessage('File cannot be sent because it is larger than 100MB.', event.threadID, () => unlinkSync(__dirname + `/cache/${rdus}.mp3`), event.messageID);
+        else return api.sendMessage({ body: desc, attachment: fs.createReadStream(__dirname + `/cache/${rdus}.mp3`) }, event.threadID, () => fs.unlinkSync(__dirname + `/cache/${rdus}.mp3`), event.messageID)
+    } catch (e) {
+        console.log(e);
+        return api.sendMessage('Your request could not be processed!', event.threadID, event.messageID);
     }
 }
+
