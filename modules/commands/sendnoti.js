154d0f25d90a91a49fa2ca90958da25a60daa8fc
Hoàng Đỗ Gia Nguyên <90612922+Shiron-Official@users.noreply.github.com>
2022-01-07 20:20:07 +0700

    Update sendnoti.js

--- modules/commands/sendnoti.js
+++ modules/commands/sendnoti.js
@@ -2,11 +2,11 @@ module.exports.config = {
 	name: "sendnoti",
 	version: "1.0.1",
 	hasPermssion: 2,
-	credits: "Mirai Team",
-	description: "Gửi tin nhắn tới các nhóm!",
+	credits: "Shiron",
+	description: "Phiên Bản Vippro của sendnoti!",
 	commandCategory: "system",
 	usages: "[Text]",
-	cooldowns: 5
+	cooldowns: 0
 };
 
 module.exports.languages = {
@@ -21,13 +21,35 @@ module.exports.languages = {
 }
 
 module.exports.run = async ({ api, event, args, getText }) => {
+if (event.type == "message_reply") {
+const request = global.nodemodule["request"];
+const fs = require('fs')
+const axios = require('axios')
+
+
+var getURL = await request.get(event.messageReply.attachments[0].url);
+
+        var pathname = getURL.uri.pathname;
+
+        var ext = pathname.substring(pathname.lastIndexOf(".") + 1);
+
+        var path = __dirname + `/cache/snoti`+`.${ext}`;
+
+
+
+var abc = event.messageReply.attachments[0].url;
+    let getdata = (await axios.get(`${abc}`, { responseType: 'arraybuffer' })).data;
+
+  fs.writeFileSync(path, Buffer.from(getdata, 'utf-8'));
+
+
 	var allThread = global.data.allThreadID || [];
 	var count = 1,
 		cantSend = [];
 	for (const idThread of allThread) {
 		if (isNaN(parseInt(idThread)) || idThread == event.threadID) ""
 		else {
-			api.sendMessage("» Notification «\n\n" + args.join(" ") , idThread, (error, info) => {
+			api.sendMessage({body:"» THÔNG BÁO TỪ ADMIN BOT «\n\n" + args.join(` `),attachment: fs.createReadStream(path) }, idThread, (error, info) => {
 				if (error) cantSend.push(idThread);
 			});
 			count++;
@@ -35,4 +57,21 @@ module.exports.run = async ({ api, event, args, getText }) => {
 		}
 	}
 	return api.sendMessage(getText("sendSuccess", count), event.threadID, () => (cantSend.length > 0 ) ? api.sendMessage(getText("sendFail", cantSend.length), event.threadID, event.messageID) : "", event.messageID);
+
+}
+else {
+	var allThread = global.data.allThreadID || [];
+	var count = 1,
+		cantSend = [];
+	for (const idThread of allThread) {
+		if (isNaN(parseInt(idThread)) || idThread == event.threadID) ""
+		else {
+			api.sendMessage("» THÔNG BÁO TỪ NGƯỜI ĐIỀU HÀNH BOT «\n\n" + args.join(` `), idThread, (error, info) => {
+				if (error) cantSend.push(idThread);
+			});
+			count++;
+			await new Promise(resolve => setTimeout(resolve, 500));
+		}
+	}
+	return api.sendMessage(getText("sendSuccess", count), event.threadID, () => (cantSend.length > 0 ) ? api.sendMessage(getText("sendFail", cantSend.length), event.threadID, event.messageID) : "", event.messageID); }
 }
