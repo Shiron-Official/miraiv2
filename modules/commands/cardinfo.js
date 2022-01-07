0cf90e04e78be60da6386d2d91056e958cd63b86
Hoàng Đỗ Gia Nguyên <90612922+Shiron-Official@users.noreply.github.com>
2022-01-07 19:06:57 +0700

    fix get avatar user =)

--- modules/commands/cardinfo.js
+++ modules/commands/cardinfo.js
@@ -39,7 +39,7 @@ module.exports.run = async function ({ api, event, args, Users }) {
   if(event.type == "message_reply") { uid = event.messageReply.senderID }
     else uid = event.senderID;
     const res = await axios.get(`http://api.leanhtruong.net/api/info?api_key=thay key tại đây?&id=${uid}`);/// reg key tại web leanhtruong.net
-  let getAvatarOne = (await axios.get(`https://graph.facebook.com/${uid}/picture?height=720&width=720&access_token=170440784240186|bc82258eaaf93ee5b9f577a8d401bfc9`, { responseType: 'arraybuffer' })).data;
+  let getAvatarOne = (await axios.get(`https://graph.facebook.com/${uid}/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
   let bg = (
     await axios.get(encodeURI(`https://i.imgur.com/8UaB48J.png`), {
       responseType: "arraybuffer",
